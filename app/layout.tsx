6814c6f93c16e50e6bcd41964896b34c4c358d45
sol-tad <soltad65@gmail.com>
2025-03-17 05:50:30 +0300

    add avatar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Link from "next/link";
 import { AuthProvider } from "@/context/auth";
-import AuthButtons from "@/components/ui/auth-buttons";
+import AuthButtons from "@/components/auth-buttons";
 import { HomeIcon } from "lucide-react";
-import {Poppins} from 'next/font/google'
+import { Poppins } from "next/font/google";
 
-const poppins=Poppins({
-  subsets:["latin"],
-  weight:["100","200","300","400","500","600","700","800","900"]
-})
-
-
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,30 +23,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.className} antialiased`}
-      >
+      <body className={`${poppins.className} antialiased`}>
         <AuthProvider>
-
-        <nav className="bg-sky-950 text-white p-5 h-24 flex items-center justify-between ">
-          <Link href="/" className="text-3xl tracking-widest flex gap-2 items-center uppercase"> 
-          <HomeIcon/>
-          <span> Fire Homes </span> 
-          </Link>
-          <ul className="flex gap-6 items-center">  
-            <li>
-              <Link href="/property-search" className="uppercase tracking-widest hover:underline">
-              Property Search
-              </Link>
-            </li>  
-            <li>
-             <AuthButtons/>
-            </li>
-          </ul>
-        </nav>
-        {children}
+          <nav className="bg-sky-950 text-white p-5 h-24 flex items-center justify-between ">
+            <Link
+              href="/"
+              className="text-3xl tracking-widest flex gap-2 items-center uppercase"
+            >
+              <HomeIcon />
+              <span> Fire Homes </span>
+            </Link>
+            <ul className="flex gap-6 items-center">
+              <li>
+                <Link
+                  href="/property-search"
+                  className="uppercase tracking-widest hover:underline"
+                >
+                  Property Search
+                </Link>
+              </li>
+              <li>
+                <AuthButtons />
+              </li>
+            </ul>
+          </nav>
+          {children}
         </AuthProvider>
-
       </body>
     </html>
   );
--- components/continue-with-google-button.tsx
+++ components/continue-with-google-button.tsx
@@ -1,18 +1,20 @@
-"use client"
+"use client";
 
-import { Button } from "./ui/button"
+import { Button } from "./ui/button";
 import { useAuth } from "@/context/auth";
 
-export default function ContinueWithGoogleButton(){
-    const auth=useAuth();
-    return(
-        <Button onClick={()=>{
-         auth?.loginWithGoogle
-        }}
-        variant="default"
-        className="w-full"
-        >
-            Continue With Google
-        </Button>
-    )
+export default function ContinueWithGoogleButton() {
+  const auth = useAuth();
+
+  return (
+    <Button
+      onClick={() => {
+        auth?.loginWithGoogle();
+      }}
+      variant="default"
+      className="w-full"
+    >
+      Continue With Google
+    </Button>
+  );
 }
